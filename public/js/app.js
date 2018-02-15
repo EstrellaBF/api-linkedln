2877ea3441057453908c2aa2938c21d5b18c5d5d
EstrellaBF <stern.von.nord>
2018-02-14 22:45:51 -0500

    subiendo autenticación e info del perfil como bienvenida

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,45 @@
 window.addEventListener('load', function () {
-  const $form = $('#form');
-  let textInput;
+  const $formSearch = $('#form-search');
+  const $profileInfo = $('#profile-info');
+  $formSearch.hide();
+  console.log($profileInfo);
 
-  $form.submit(function (e) {
-    e.preventDefault();
-    console.log(e.target);
-    textInput = $form.find('input').val()
-    console.log(textInput);
-    getEnterprise();
-  });
+  // Hace una llamada al api cuando la autenticación esta completa
+  function onLinkedInLoad() {
+    IN.Event.on(IN, "auth", getProfileData);
+    console.log();
+  }
+  console.log(onLinkedInLoad());
 
-  function getEnterprise(){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET','https://api.linkedin.com/v1/people/~?format=json');
-    xhr.onload = addNews;
-    // xhr.onerror = handleError;
-    xhr.send();
-    console.log(xhr);
+  // Deslogea al usuaro de la cuenta de Linkedin
+  function logout() {
+    IN.User.logout(onLogout);
+  }
+  console.log(logout());
+
+  function onLogout() {
+    console.log('Logout successfully');
   }
 
+  // Use the API call wrapper to request the member's basic profile data
+  function getProfileData() {
 
-  // Función addNews
-  function addNews(e) {
-    console.log(e.target.responseText);
-    const data = JSON.parse(e.target.responseText)
-    console.log(data);
+    IN.API.Profile("me").fields("first-name", "last-name", "email-address", "picture-url").result(function (data) {
 
+      var userdata = data.values[0];
+      var fname = userdata.firstName;
+      var lname = userdata.lastName;
+      var email = userdata.emailAddress;
+      // console.log(profile_photo); // undefined
+      // Añadiendo a la página
+    $profileInfo.append(`<h3>Bienvenida ${fname} ${lname}</h3>`);
+    $formSearch.show();
+    $('span').hide();
+
+      logout();
+    }).error(function (data) {
+      console.log(data);
+    });
   }
 
 });
