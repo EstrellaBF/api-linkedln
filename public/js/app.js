75a3bb754db975f067ac85bfab3a198d65d99b77
EstrellaBF <stern.von.nord>
2018-02-15 11:40:01 -0500

    añadiendo estilos

--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,37 @@
 window.addEventListener('load', function () {
+  // $(document).ready(function () {
   const $formSearch = $('#form-search');
   const $profileInfo = $('#profile-info');
   let member;
 
+
   $formSearch.hide();
+  // Removing event listener by calling IN.Event.on() outside of onLinkedInLoad()
 
-  // Hace una llamada al api cuando la autenticación esta completa
-  function onLinkedInLoad() {
-    IN.Event.on(IN, "auth", getProfileData);
-    console.log();
-  }
-  console.log(onLinkedInLoad());
-
-  // Deslogea al usuaro de la cuenta de Linkedin
-  function logout() {
-    IN.User.logout(onLogout);
-  }
-  console.log(logout());
-
-  function onLogout() {
-    console.log('Logout successfully');
+  IN.Event.on(IN, "auth", getProfileData);
+  // Handle the successful return from the API call
+  function onSuccess(data) {
+    console.log(data);
+    console.log(data.firstName);
+    console.log(data.lastName);
+    console.log(data.headline);
+    // Añadiendo a la página
+    $profileInfo.append(`<h3>PROFILE LINKEDIN</h3><p>${data.firstName} ${data.lastName}</p><p>${data.headline}</p>`);
+    $formSearch.show();
+    $('span').hide();
+    $('#login-text').hide();
   }
 
-  function displayProfiles(profiles) {
-    console.log(profiles);
-    member = profiles.values[0];
-    console.log(member);
+  // Handle an error response from the API call
+  function onError(error) {
+    console.log(error);
   }
-
-
 
   // Use the API call wrapper to request the member's basic profile data
   function getProfileData() {
-
-    IN.API.Profile("me").fields("first-name", "last-name", "email-address", "picture-url").result(function (data) {
-
-      var userdata = data.values[0];
-      var fname = userdata.firstName;
-      var lname = userdata.lastName;
-      var email = userdata.emailAddress;
-      // console.log(profile_photo); // undefined
-      // Añadiendo a la página
-      $profileInfo.append(`<h3>Bienvenida ${fname} ${lname}</h3>`);
-      $formSearch.show();
-      $('span').hide();
-
-      logout();
-    }).error(function (data) {
-      console.log(data);
-    });
+    IN.API.Raw("/people/~").result(onSuccess).error(onError);
   }
 
+
 });
+
