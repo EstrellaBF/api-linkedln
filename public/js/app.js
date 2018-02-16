79672f12ae0e2193ed78aaad045358baf5a3ce6b
EstrellaBF <stern.von.nord>
2018-02-16 00:16:51 -0500

    subiendo css a la información de perfil

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,5 @@
 window.addEventListener('load', function () {
 
-
-  // Setup an event listener to make an API call once auth is complete
-  // function onLinkedInLoad() {
-  //   IN.Event.on(IN, "auth", getProfileData);
-  // }
-
-
   IN.Event.on(IN, "auth", getProfileData);
 
   // Logout user
@@ -27,11 +20,11 @@ window.addEventListener('load', function () {
         // console.log(userdata.positions.values);
         $('#login-text').hide();
         // Añadiendo al DOM
-        $('#about-webapp').append(`<div class="row"><div class="col s12 col m12 col l10">
+        $('#about-webapp').prepend(`<div class="row"><div class="col s12 m12 l10">
         <div class="cv-page">
         <div class="name"><img src="${userdata.pictureUrl}"><h2>${userdata.firstName} ${userdata.lastName}</h2></div>
         <div class="info"><p>Email: ${userdata.emailAddress}</p><p>About me: ${userdata.summary}</p><p>Industry: ${userdata.industry}</p><p>Connections at Linkedin: ${userdata.numConnections}</p></div>
-        <div class="jobs"><p>Last job: ${userdata.positions.values[0].company.name}</p><p>Position :${userdata.positions.values[0].title}</p></div>
+        <div class="job"><p>Last job: ${userdata.positions.values[0].company.name}</p><p>Position :${userdata.positions.values[0].title}</p></div>
         <div></div>
         </div></div></div>`);
       }).error(function(data) {
